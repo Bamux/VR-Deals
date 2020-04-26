bd91b2052b19c55cfe2964e168b8cd025f114552
Bamu <bamueq@googlemail.com>
2020-04-26 14:34:19 +0200

    update

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@
 # Database Connection
 host = "localhost"
 user = "root"
-passwd = " "  # enter your password to your mysql database here
+passwd = ""  # enter your password to your mysql database here
 database = "vr_dealz"
