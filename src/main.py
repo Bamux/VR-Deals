f1f5003ff21565f9c1088967a048463913e4ce0b
Bamu <bamueq@googlemail.com>
2020-04-26 16:38:55 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import sql
 import get_offers
+from pathlib import Path
 
 
 def main():
-    offers = get_offers.main()
-    sql.add_current_offers(offers)
-    sql.cursor.close()
-    sql.mydb.close()
-    print("test")
+    if Path("settings.py"):
+        offers = get_offers.main()
+        sql.add_current_offers(offers)
+        sql.cursor.close()
+        sql.mydb.close()
+    else:
+        print("Please fill out the settings.example.py and rename it to settings.py")
 
 
 if __name__ == "__main__":
