931c91a5fd6bf3fb26f1ed3ee457a0e4b6ca50a1
Bamu <bamueq@googlemail.com>
2020-04-28 12:37:05 +0200

    update

--- database/database.sql
+++ database/database.sql
@@ -16,14 +16,26 @@
 CREATE DATABASE IF NOT EXISTS `vr_dealz` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_german2_ci */;
 USE `vr_dealz`;
 
+-- Exportiere Struktur von Tabelle vr_dealz.articles
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) NOT NULL,
+  `website_article_id` bigint(20) NOT NULL,
+  `article_name` tinytext COLLATE utf8_german2_ci NOT NULL,
+  `regular_price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+
+-- Exportiere Daten aus Tabelle vr_dealz.articles: ~0 rows (ungefähr)
+DELETE FROM `articles`;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+
 -- Exportiere Struktur von Tabelle vr_dealz.current_offers
 CREATE TABLE IF NOT EXISTS `current_offers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `store_id` bigint(20) NOT NULL DEFAULT 0,
-  `title` tinytext COLLATE utf8_german2_ci NOT NULL,
-  `sale_price` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `regular_price` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `headset` tinytext COLLATE utf8_german2_ci NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `sale_price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
@@ -31,6 +43,40 @@ CREATE TABLE IF NOT EXISTS `current_offers` (
 DELETE FROM `current_offers`;
 /*!40000 ALTER TABLE `current_offers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `current_offers` ENABLE KEYS */;
+
+-- Exportiere Struktur von Tabelle vr_dealz.headsets
+CREATE TABLE IF NOT EXISTS `headsets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8_german2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+
+-- Exportiere Daten aus Tabelle vr_dealz.headsets: ~4 rows (ungefähr)
+DELETE FROM `headsets`;
+/*!40000 ALTER TABLE `headsets` DISABLE KEYS */;
+INSERT INTO `headsets` (`id`, `name`) VALUES
+	(1, 'Rift'),
+	(2, 'Rift S'),
+	(3, 'Quest'),
+	(4, 'Go');
+/*!40000 ALTER TABLE `headsets` ENABLE KEYS */;
+
+-- Exportiere Struktur von Tabelle vr_dealz.stores
+CREATE TABLE IF NOT EXISTS `stores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8_german2_ci NOT NULL,
+  `url` text COLLATE utf8_german2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+
+-- Exportiere Daten aus Tabelle vr_dealz.stores: ~2 rows (ungefähr)
+DELETE FROM `stores`;
+/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+INSERT INTO `stores` (`id`, `name`, `url`) VALUES
+	(1, 'Oculus Quest Store', 'https://www.oculus.com/experiences/quest/'),
+	(2, 'Oculus Rift Store', 'https://www.oculus.com/experiences/rift/section/1578364258944223/'),
+	(3, 'Oculus Go Store', 'https://www.oculus.com/experiences/go/section/1500175860035862/');
+/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
