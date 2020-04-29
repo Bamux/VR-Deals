d4295bfd4455ed071a17d49150a5a398003d66e5
Bamu <bamueq@googlemail.com>
2020-04-29 20:30:11 +0200

    update

--- src/database/database.sql
+++ src/database/database.sql
@@ -12,11 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Exportiere Datenbank Struktur für vr_dealz
-CREATE DATABASE IF NOT EXISTS `vr_dealz` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_german2_ci */;
-USE `vr_dealz`;
+-- Exportiere Datenbank Struktur für vr_schnaeppchen
+CREATE DATABASE IF NOT EXISTS `vr_schnaeppchen` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_german2_ci */;
+USE `vr_schnaeppchen`;
 
--- Exportiere Struktur von Tabelle vr_dealz.articles
+-- Exportiere Struktur von Tabelle vr_schnaeppchen.articles
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `store_id` int(11) NOT NULL,
@@ -26,12 +26,12 @@ CREATE TABLE IF NOT EXISTS `articles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
--- Exportiere Daten aus Tabelle vr_dealz.articles: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle vr_schnaeppchen.articles: ~0 rows (ungefähr)
 DELETE FROM `articles`;
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 
--- Exportiere Struktur von Tabelle vr_dealz.current_offers
+-- Exportiere Struktur von Tabelle vr_schnaeppchen.current_offers
 CREATE TABLE IF NOT EXISTS `current_offers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NOT NULL,
@@ -40,12 +40,12 @@ CREATE TABLE IF NOT EXISTS `current_offers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
--- Exportiere Daten aus Tabelle vr_dealz.current_offers: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle vr_schnaeppchen.current_offers: ~0 rows (ungefähr)
 DELETE FROM `current_offers`;
 /*!40000 ALTER TABLE `current_offers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `current_offers` ENABLE KEYS */;
 
--- Exportiere Struktur von Tabelle vr_dealz.expired_offers
+-- Exportiere Struktur von Tabelle vr_schnaeppchen.expired_offers
 CREATE TABLE IF NOT EXISTS `expired_offers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NOT NULL,
@@ -54,19 +54,19 @@ CREATE TABLE IF NOT EXISTS `expired_offers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
--- Exportiere Daten aus Tabelle vr_dealz.expired_offers: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle vr_schnaeppchen.expired_offers: ~0 rows (ungefähr)
 DELETE FROM `expired_offers`;
 /*!40000 ALTER TABLE `expired_offers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `expired_offers` ENABLE KEYS */;
 
--- Exportiere Struktur von Tabelle vr_dealz.headsets
+-- Exportiere Struktur von Tabelle vr_schnaeppchen.headsets
 CREATE TABLE IF NOT EXISTS `headsets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext COLLATE utf8_german2_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
--- Exportiere Daten aus Tabelle vr_dealz.headsets: ~4 rows (ungefähr)
+-- Exportiere Daten aus Tabelle vr_schnaeppchen.headsets: ~4 rows (ungefähr)
 DELETE FROM `headsets`;
 /*!40000 ALTER TABLE `headsets` DISABLE KEYS */;
 INSERT INTO `headsets` (`id`, `name`) VALUES
@@ -76,7 +76,7 @@ INSERT INTO `headsets` (`id`, `name`) VALUES
 	(4, 'Go');
 /*!40000 ALTER TABLE `headsets` ENABLE KEYS */;
 
--- Exportiere Struktur von Tabelle vr_dealz.stores
+-- Exportiere Struktur von Tabelle vr_schnaeppchen.stores
 CREATE TABLE IF NOT EXISTS `stores` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext COLLATE utf8_german2_ci NOT NULL,
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `stores` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
--- Exportiere Daten aus Tabelle vr_dealz.stores: ~2 rows (ungefähr)
+-- Exportiere Daten aus Tabelle vr_schnaeppchen.stores: ~2 rows (ungefähr)
 DELETE FROM `stores`;
 /*!40000 ALTER TABLE `stores` DISABLE KEYS */;
 INSERT INTO `stores` (`id`, `name`, `url`) VALUES
