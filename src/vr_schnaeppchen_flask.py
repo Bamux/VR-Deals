6e8c45bd09fd8839f30c95301fc0750ae1268fe3
Bamu <bamueq@googlemail.com>
2020-04-30 17:22:33 +0200

    update

--- src/vr_schnaeppchen_flask.py
+++ src/vr_schnaeppchen_flask.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
+import settings
 
 app = Flask(__name__)
-
-card_title = "Card Title"
-card = "This is a wider card with supporting text below as a natural lead-in to " \
-       "additional content. This content is a little bit longer."
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = settings.user
+app.config['MYSQL_DATABASE_PASSWORD'] = settings.passwd
+app.config['MYSQL_DATABASE_DB'] = settings.database
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html', card_title=card_title, card=card)
+    cursor.execute('''
+    SELECT article_name, sale_price, regular_price FROM current_offers 
+    INNER JOIN articles ON articles.id = current_offers.article_id''')
+    articles = cursor.fetchall()
+    return render_template('home.html', articles=articles)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
