6162ed19baa00ec76c018b83f9e98a3716c9e93e
Bamu <bamueq@googlemail.com>
2020-05-19 13:58:20 +0200

    update

--- src/flask_vrdeals/routes.py
+++ src/flask_vrdeals/routes.py
@@ -1,69 +1,38 @@
-from flask import render_template  # , make_response
+from flask import render_template
 
 from flask_vrdeals import app, database
 
 
-# @app.route("/")
-# def index():
-#     offers, pagination = database.offers_pagination()
-#     return render_template('content.html', page_navigation="Home", offers=offers, pagination=pagination)
-
 @app.route("/", defaults={'page': 1})
 @app.route("/page-<int:page>/")
 def index(page):
-    if page > 0:
-        offers, pages, data_to_show = database.offers_pagination(page)
-        return render_template('content.html', page_navigation="Home", offers=offers,
-                               pages=pages, data_to_show=data_to_show)
-    else:
-        return "<h1>Error</h1>"
+    offers, pages, data_to_show = database.offers_pagination(page)
+    return render_template('content.html', page_navigation="Home", offers=offers,
+                           pages=pages, data_to_show=data_to_show)
 
 
 @app.route("/humble/", defaults={'page': 1})
 @app.route("/humble/page-<int:page>/")
 def humble(page):
-    if page > 0:
-        offers, pages, data_to_show = database.offers_pagination(page, "Humble Bundle")
-        return render_template('content.html', page_navigation="Humble Bundle", offers=offers,
-                               pages=pages, data_to_show=data_to_show)
-    else:
-        return "<h1>Error</h1>"
+    offers, pages, data_to_show = database.offers_pagination(page, "Humble Bundle")
+    return render_template('content.html', page_navigation="Humble Bundle", offers=offers,
+                           pages=pages, data_to_show=data_to_show)
 
 
 @app.route("/oculus/", defaults={'page': 1})
 @app.route("/oculus/page-<int:page>/")
 def oculus(page):
-    if page > 0:
-        offers, pages, data_to_show = database.offers_pagination(page, "Oculus")
-        return render_template('content.html', page_navigation="Oculus", offers=offers,
-                               pages=pages, data_to_show=data_to_show)
-    else:
-        return "<h1>Error</h1>"
+    offers, pages, data_to_show = database.offers_pagination(page, "Oculus")
+    return render_template('content.html', page_navigation="Oculus", offers=offers,
+                           pages=pages, data_to_show=data_to_show)
 
 
 @app.route("/steam/", defaults={'page': 1})
 @app.route("/steam/page-<int:page>/")
 def steam(page):
-    if page > 0:
-        offers, pages, data_to_show = database.offers_pagination(page, "Steam")
-        return render_template('content.html', page_navigation="Steam", offers=offers,
-                               pages=pages, data_to_show=data_to_show)
-    else:
-        return "<h1>Error</h1>"
-
-
-# @app.route("/oculus/")
-# def oculus():
-#     # test = request.args.get('page', default=1, type=int)
-#     # print(test)
-#     offers, pagination = database.offers_pagination("Oculus")
-#     return render_template('content.html', page_navigation="Oculus", offers=offers, pagination=pagination)
-#
-#
-# @app.route("/steam/")
-# def steam():
-#     offers, pagination = database.offers_pagination("Steam")
-#     return render_template('content.html', page_navigation="Steam", offers=offers, pagination=pagination)
+    offers, pages, data_to_show = database.offers_pagination(page, "Steam")
+    return render_template('content.html', page_navigation="Steam", offers=offers,
+                           pages=pages, data_to_show=data_to_show)
 
 
 # @app.errorhandler(404)
