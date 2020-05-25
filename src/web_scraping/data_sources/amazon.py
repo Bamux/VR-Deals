180f4cc7e9013036ab3c51bc680accbc4ca9d0d1
Bamu <bamueq@googlemail.com>
2020-05-25 23:24:35 +0200

    update

---
+++ src/web_scraping/data_sources/amazon.py
@@ -0,0 +1,62 @@
+from decimal import Decimal
+
+import requests
+from bs4 import BeautifulSoup
+
+from web_scraping.data_sources.article import Article, check_keywords
+from web_scraping import sql
+
+
+def get_amazon_offers(store_id):
+    offers = []
+    url = "https://www.amazon.de/s?k=virtual+reality+brille"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                      "(KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.3",
+        "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+                  "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Connection": "keep-alive",
+        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3"
+    }
+    html = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(html, 'lxml')
+    soup = soup.find('div', class_='s-main-slot s-result-list s-search-results sg-row')
+    soup = soup.find_all('div', class_='s-expand-height s-include-content-margin s-border-bottom s-latency-cf-section')
+    for article in soup:
+        article_name = article.find('span', class_='a-size-base-plus a-color-base a-text-normal').text
+        sale_price = article.find('span', class_='a-price-whole')
+        keyword = check_keywords(article_name)
+        if sale_price and keyword:
+            category_id, article_name, regular_price = keyword
+            sale_price = Decimal(sale_price.text.replace(',', '.'))
+            if sale_price < regular_price:
+                website_article_id = article.find('a', class_='a-link-normal a-text-normal')
+                website_article_id = website_article_id['href'].split("ref=")[0]
+                img_url = article.find('img')
+                img_url = img_url.get('srcset').split(",")[2].split(" ")[1]
+                article_id = 0
+                offer = Article(
+                    article_id,
+                    store_id,
+                    category_id,
+                    website_article_id,
+                    article_name,
+                    regular_price,
+                    sale_price,
+                    img_url,
+                )
+                offers.append(offer)
+                offer.print_offer()
+    return offers
+
+
+def main():
+    print("\nAmazon:\n")
+    store_id = sql.get_store_id("Amazon")[0]
+    offers = get_amazon_offers(store_id)
+    return offers
+
+
+if __name__ == "__main__":
+    main()
